e1961ed0b41df13e03352feab0232ada2deb07de
João Alberto <joaoalbertoxtz@gmail.com>
2013-03-22 12:11:23 -0300

    Update jcaption.js

--- jcaption.js
+++ jcaption.js
@@ -8,28 +8,37 @@
  * @license CC Attribution-Share Alike 3.0 - http://creativecommons.org/licenses/by-sa/3.0/
  */
 
-(function($) {
+; (function($) {
     $.fn.jcaption = function(options) {
         /* DEFAULTS */
         var defaults = {
-            randomColor : false,    		/* Boolean */
-            showBorder : true,    			/* Boolean */
-            lengthBorder : 2,   			/* String  */
-            borderStyle : 'solid',			/* String  */
-            borderColor : 'rgb(34,34,34)',	/* String  */
-           	defaultColor: 'rgb(30,100,200)'
+            randomColor : false,
+            Color: 'rgb(30,100,200)',
+            animationHeight: '100%',
+            opacity: 1,
+            animationSpeed: 700
         }
+        
         var setings = $.extend( {}, defaults, options);
+        
         return this.each (function() {
+            var tagName = $(this).children().prop("tagName").toLowerCase();
+            var imgAttrTitle = $(this).children().attr('title');
             
-            var tagName, imgAttrTitle;
-            tagName = $(this).children().prop("tagName").toLowerCase(); /* procura os elementos que são img*/
-            imgAttrTitle = $(this).children().attr('title'); /* pega o valor do atributo title da imagem */
+            if( tagName == "img" && imgAttrTitle != "") {
+                $(this).children().wrap("<div class='center-caption'></div>").after("<div class='caption' >"+ imgAttrTitle +"</div>").attr("title", '');    
+			} /* fim do if */
             
-            if( tagName == "img" && imgAttrTitle != "") { /* Testa se o elemento é uma img e se o attr title esta setado */
-                /* Se o if for verdadeiro engloba a imagem em uma div e adiciona um elemento span com o valor do title da imagem */
-                $(this).children().wrap("<div class='center-caption'></div>").before("<span class='caption'>"+ imgAttrTitle +"</span>");
-            } /* fim do if */
+            var obj = $('img.caption-receive');
+			$('.center-caption').width( $(obj).width() + parseInt($(obj).css('padding-left')) + parseInt($(obj).css('padding-right')) + parseInt($(obj).css('borderLeftWidth')) + parseInt($(obj).css('borderRightWidth'))).css('float', $(obj).css('float'));
+            
+            $('.center-caption').css({'position' : 'relative'});
+            $('.caption').css({ 'position' : 'absolute' , 'bottom' : '0' , 'left' : '0'});
+            $(obj).css('position','relative');
+            
+            $('.center-caption').css({ 'margin' : $(obj).css('margin-top') + ' ' + $(obj).css('margin-right') + ' ' + $(obj).css('margin-bottom') + ' ' + $(obj).css('margin-left')});
+            $(obj).css('margin', '0');
+            
         }); /* fim do return this */
     }; /* fim do jcaption */
 }) ( jQuery );
